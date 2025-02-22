ff38c64b628ddff1d4e803138dc3638e47002834
NilInstance <braindim@gmail.com>
2025-02-22 13:17:24 +0200

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId = "10449761463" then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NilInstances/Horizon/refs/heads/main/TSB.lua"))()
+else
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NilInstances/Horizon/refs/heads/main/Universal.lua"))()
+end
